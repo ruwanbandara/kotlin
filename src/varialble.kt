7dd39ad64d9af9cc39c990cd36e89962a2db9e8c
ruwanbandara <ruwan.se.uok@gmail.com>
2019-09-27 12:35:35 +0530

    koltin variable

--- src/varialble.kt
+++ src/varialble.kt
@@ -1,5 +1,14 @@
 fun main(args: Array<String>){
     var myString = "Hello word"
 
+    var myNumber = 10
+    var myDecimal = 1.0
+
+    var myString: String //String
+    myString = "Another Word"
+
+    val myAnotherStr = "My constant string value"//Constant Value
+
     print(myString)
+
 }
