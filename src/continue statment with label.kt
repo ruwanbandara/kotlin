ec7c157e35e3c96f0f3178fceea273ca9c68a025
ruwanbandara <ruwan.se.uok@gmail.com>
2019-10-01 14:19:53 +0530

    continue statment with label

--- src/continue statment with label.kt
+++ src/continue statment with label.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>){
-    for (i in 1..3){
+    outer@for (i in 1..3){
         for(j in 1..3){
             if(i==2 && j==2)
-                continue
+                continue@outer
             println("$i $j")
         }
     }
