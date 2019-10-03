c9ed771efee5de9e5759383612214e6a055dda87
ruwanbandara <ruwan.se.uok@gmail.com>
2019-10-03 14:42:48 +0530

    named parameter

---
+++ src/named parameter.kt
@@ -0,0 +1,18 @@
+fun main (args: Array<String>){
+
+
+    findvolume(length = 50,height = 80,breadth = 10)
+
+}
+
+fun findvolume(length:Int,breadth:Int,height:Int){
+
+    println("Length is" +length)
+    println("breadth is" +breadth)
+    println("height is" +height)
+}
+
+//out put
+//Length is50
+//breadth is10
+//height is80
