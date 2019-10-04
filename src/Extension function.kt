9ac7ebc3ef375ffbbe17174188177feaf9fa847d
ruwanbandara <ruwan.se.uok@gmail.com>
2019-10-04 17:38:28 +0530

    Extension function

--- src/Extension function.kt
+++ src/Extension function.kt
@@ -6,8 +6,24 @@ fun main(args: Array<String>){
 
 
     println(str3.add(str1 ,str2))
+
+
+
+//...................................................//
+
+
+    val x:Int = 6
+    val y:Int =12
+    val greaterVal = x.greaterValue(y)
+    println(greaterVal)
 }
 
 fun String.add(s1:String, s2:String):String{
     return this + s1 +s2
+}
+fun Int.greaterValue(other:Int):Int{
+    if(this>other)
+        return this
+    else
+        return other
 }
