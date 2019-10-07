78c54264170306089de9870f67ec2d8a458a5eb3
ruwanbandara <ruwan.se.uok@gmail.com>
2019-10-07 22:12:02 +0530

    Lambdas

--- src/Lambdas.kt
+++ src/Lambdas.kt
@@ -15,9 +15,18 @@ package com.kotlin
          }
 
      })
+
+     var test: String = "Hello"
+     val myLambda: (Int)-> Unit  = {s: Int -> println(s)}
+     program.addTwoNumbers(2,7, myLambda)
  }
 
 class Program{
+
+    fun addTwoNumbers(a:Int , b:Int,action: (Int) -> Unit){ // High level function with Lambda as parameter
+        val sum = a+b
+        println(sum)
+    }
 
     fun addTwoNumbers(a:Int , b:Int, action:MyInterface1){  // Using Interface / object orinted way
         val sum = a+b
