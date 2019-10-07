8522f41cca8063d249fb3f62f24b92a0aa8905a4
ruwanbandara <ruwan.se.uok@gmail.com>
2019-10-07 22:40:19 +0530

    Lambdas and High Level Function

---
+++ src/Lambda and high level function.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>){
+    val program1 = Program1()
+
+    val MyLambda:(Int,Int)-> Int ={x:Int,y:Int -> x+y} //Lambda Expression[Function]
+    program1.addTwoNumbers1(2,7,MyLambda)
+
+
+}
+
+class Program1{
+    fun addTwoNumbers1(a:Int,b:Int, action: (Int,Int)-> Int){ // High Level function with Lambda
+        val result = action(a,b) //x+y =a+b = 2+7 = 9
+        println(result)
+    }
+}
