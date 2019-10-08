25914a7e1906eb924864e40cb3eb5d62b9dcfe69
ruwanbandara <ruwan.se.uok@gmail.com>
2019-10-08 16:31:23 +0530

    List and Array

---
+++ src/List and array.kt
@@ -0,0 +1,8 @@
+fun main(array: Array<String>){
+
+    var list = listOf<String>("Ruwan","Bandara","Wijepala") //immutable,Fixed Size,Read Only\
+
+    for(index in 0..list.size -1){
+     println(list[index])
+}
+}
