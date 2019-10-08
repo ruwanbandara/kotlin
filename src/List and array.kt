2dabbfcc9b320aac53701f837e3fe4847d71ca6a
ruwanbandara <ruwan.se.uok@gmail.com>
2019-10-08 19:54:12 +0530

    List in Kotlin

--- src/List and array.kt
+++ src/List and array.kt
@@ -2,7 +2,18 @@ fun main(array: Array<String>){
 
     var list = listOf<String>("Ruwan","Bandara","Wijepala") //immutable,Fixed Size,Read Only\
 
+
+
+
     for(index in 0..list.size -1){
      println(list[index])
+
+    }
+
+    println()
+
+    for (element in list){
+        println(element)
+    }
 }
-}
+
