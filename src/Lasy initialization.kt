12aeac819732ae39958722bd00e24da25d855b01
ruwanbandara <ruwan.se.uok@gmail.com>
2019-10-09 10:49:06 +0530

    Lasy Initization THE FNAL COMMIT

---
+++ src/Lasy initialization.kt
@@ -0,0 +1,14 @@
+val pi: Float by lazy {
+    3.14f
+}
+
+fun main (args: Array<String>){
+    println("Some inilal code ..")
+
+    val area1 = pi*4*4
+
+    val area2 = pi*9*9
+
+    println(area1)
+    println(area2)
+}
